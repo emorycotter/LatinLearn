81c2859c5b9e7d41bcc0f73c6ad1988855b056d7
emorycotter <79954300+emorycotter@users.noreply.github.com>
2022-12-05 07:08:17 -0600

    Initial Commit

---
+++ LatinLearnProject/LatinLearnProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatinLearnProjectApp.swift
+//  LatinLearnProject
+//
+//  Created by Emory Cotter on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LatinLearnProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
